aa5b1d4f59f10d1e3bff27517a84c0c5f155bd36
santanarscs <raphaelstn@gmail.com>
2020-08-24 17:21:44 -0300

    feat: add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Content } from './styles';
+
+export const Header: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <h1>POC-CAMUNDA</h1>
+      </Content>
+    </Container>
+  );
+};
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 70px;
+  width: 100%;
+  background: #2c3e50;
+`;
+
+export const Content = styled.nav`
+  height: 100%;
+  max-width: 1100px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  h1 {
+    color: #ffff;
+  }
+`;
