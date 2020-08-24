8c3d09d81eb73755ec5eb87854fcf8c6d8263a10
santanarscs <raphaelstn@gmail.com>
2020-08-24 17:22:23 -0300

    feat: configure global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from './routes';
 
+import GlobalStyle from './styles/global';
+import { Header } from './components/Header';
+
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <Header />
+
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </>
   );
 };
 
