6a696ce1e6549ae42e6f59aaee631a8b2365f627
santanarscs <raphaelstn@gmail.com>
2020-08-27 11:52:23 -0300

    feat: upate header, create mock indicators

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,40 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-import { FiSettings } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
-import api from '../../services/api';
-import { IProcessDefinition } from '../../interfaces/IProcessDefinition';
-
-import { Container, DeploymentList } from './styles';
+import { Container, Card, CardList } from './styles';
+import { Header } from '../../components/Header';
 
 export const Dashboard: React.FC = () => {
-  const [processes, setProcesses] = useState<IProcessDefinition[]>();
-
-  useEffect(() => {
-    async function loadDeployments() {
-      const response = await api.get<IProcessDefinition[]>(
-        'process-definition?latest=true&active=true',
-      );
-      setProcesses(response.data);
-    }
-    loadDeployments();
-  }, []);
   return (
-    <Container>
-      <h1>Processos disponíveis</h1>
-      <DeploymentList>
-        {processes?.map(process => (
-          <Link key={process.id} to={`/start-process/key/${process.key}`}>
-            <li>
-              <FiSettings size={35} />
-              <div>
-                <h3>{process.name}</h3>
-                <small>{process.versionTag}</small>
-              </div>
-            </li>
-          </Link>
-        ))}
-      </DeploymentList>
-    </Container>
+    <>
+      <Header />
+      <Container>
+        <h1>Dashboard</h1>
+        <CardList>
+          <Card>
+            <strong>Solicitações abertas</strong>
+            <span>32</span>
+          </Card>
+          <Card>
+            <strong>Tarefas em andamento</strong>
+            <span>123</span>
+          </Card>
+        </CardList>
+      </Container>
+    </>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -8,35 +8,28 @@ export const Container = styled.div`
   }
 `;
 
-export const DeploymentList = styled.ul`
+export const CardList = styled.ul`
   margin-top: 30px;
   list-style: none;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 10px;
   color: #4d4d44;
-  svg {
-    margin-right: 20px;
-    transition: all 1s;
-  }
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-  li {
-    background: #fff;
-    padding: 20px;
-    border-radius: 5px;
-    min-height: 100px;
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-    transition: box-shadow 0.2s;
-    &:hover {
-      box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-      svg {
-        transform: rotate(180deg);
-      }
+`;
+export const Card = styled.li`
+  background: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  min-height: 100px;
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
+  transition: box-shadow 0.2s;
+
+  &:hover {
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    svg {
+      transform: rotate(180deg);
     }
   }
 `;
