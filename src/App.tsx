904712bf8b50e1234667d3aecb90b2ae51669ecc
santanarscs <raphaelstn@gmail.com>
2020-08-27 11:41:35 -0300

    feat: extends route react-router-dom for private routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { Routes } from './routes';
 
 import GlobalStyle from './styles/global';
-import { Header } from './components/Header';
 
 const App: React.FC = () => {
   return (
     <>
       <GlobalStyle />
-
       <BrowserRouter>
-        <Header />
-
         <Routes />
       </BrowserRouter>
     </>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import { Route } from './Route';
 
 import { Dashboard } from '../pages/Dashboard';
 import { Deployments } from '../pages/Deployments';
 import { StartProcess } from '../pages/StartProcess';
+import { Login } from '../pages/Login';
 
 export const Routes: React.FC = () => {
   return (
     <Switch>
-      <Route path="/start-process/key/:key" component={StartProcess} />
-      <Route path="/deployments" component={Deployments} />
-      <Route path="/" exact component={Dashboard} />
+      <Route
+        path="/start-process/key/:key"
+        component={StartProcess}
+        isPrivate
+      />
+      <Route path="/deployments" component={Deployments} isPrivate />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/" exact component={Login} />
     </Switch>
   );
 };
