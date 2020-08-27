36c6f5947becbb044f25e30f9e01b408eda6e9e1
santanarscs <raphaelstn@gmail.com>
2020-08-27 15:04:34 -0300

    feat: create mock page users

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,14 +1,134 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { FiMenu, FiEdit, FiTrash, FiEye } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import {
+  Container,
+  HeaderContent,
+  Content,
+  UserList,
+  UserItem,
+  MenuActionItem,
+} from './styles';
 import { Header } from '../../components/Header';
+import Dropdown from '../../components/Dropdown';
 
 export const Users: React.FC = () => {
   return (
     <>
       <Header />
       <Container>
-        <h1>Usuários</h1>
+        <HeaderContent>
+          <h1>Usuários</h1>
+          <Link to="/users/new">Novo usuário</Link>
+        </HeaderContent>
+
+        <Content>
+          <UserList>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>Status</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <UserItem>
+              <td />
+              <td>Raphael</td>
+              <td>raphaelstn@gmail.com</td>
+              <td>Ativo</td>
+              <td>
+                <Dropdown icon={FiMenu}>
+                  <MenuActionItem>
+                    <Link to="!">
+                      <FiEye />
+                      <span>Detalhes</span>
+                    </Link>
+                  </MenuActionItem>
+                  <MenuActionItem>
+                    <Link to="!">
+                      <FiEdit />
+                      <span>Editar</span>
+                    </Link>
+                  </MenuActionItem>
+                  <MenuActionItem>
+                    <button
+                      type="button"
+                      onClick={() => console.log('lkajsdf')}
+                    >
+                      <FiTrash />
+                      <span>Remover</span>
+                    </button>
+                  </MenuActionItem>
+                </Dropdown>
+              </td>
+            </UserItem>
+            <UserItem>
+              <td />
+              <td>Fernando</td>
+              <td>fernando_tomate@gmail.com</td>
+              <td>Ativo</td>
+              <td>
+                <Dropdown icon={FiMenu}>
+                  <MenuActionItem>
+                    <Link to="!">
+                      <FiEye />
+                      <span>Detalhes</span>
+                    </Link>
+                  </MenuActionItem>
+                  <MenuActionItem>
+                    <Link to="!">
+                      <FiEdit />
+                      <span>Editar</span>
+                    </Link>
+                  </MenuActionItem>
+                  <MenuActionItem>
+                    <button
+                      type="button"
+                      onClick={() => console.log('lkajsdf')}
+                    >
+                      <FiTrash />
+                      <span>Remover</span>
+                    </button>
+                  </MenuActionItem>
+                </Dropdown>
+              </td>
+            </UserItem>
+            <UserItem>
+              <td />
+              <td>Cleiton</td>
+              <td>dev.cleiton@gmail.com</td>
+              <td>Desativado</td>
+              <td>
+                <Dropdown icon={FiMenu}>
+                  <MenuActionItem>
+                    <Link to="!">
+                      <FiEye />
+                      <span>Detalhes</span>
+                    </Link>
+                  </MenuActionItem>
+                  <MenuActionItem>
+                    <Link to="!">
+                      <FiEdit />
+                      <span>Editar</span>
+                    </Link>
+                  </MenuActionItem>
+                  <MenuActionItem>
+                    <button
+                      type="button"
+                      onClick={() => console.log('lkajsdf')}
+                    >
+                      <FiTrash />
+                      <span>Remover</span>
+                    </button>
+                  </MenuActionItem>
+                </Dropdown>
+              </td>
+            </UserItem>
+          </UserList>
+        </Content>
       </Container>
     </>
   );
--- src/pages/Users/styles.ts
+++ src/pages/Users/styles.ts
@@ -7,3 +7,81 @@ export const Container = styled.div`
     color: #353535;
   }
 `;
+
+export const HeaderContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 20px;
+  a {
+    border: 0;
+    border-radius: 5px;
+    color: #fff;
+    background: #3498db;
+    height: 50px;
+    width: 180px;
+    text-transform: uppercase;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  background: #fff;
+  padding: 20px;
+  border-radius: 5px;
+`;
+
+export const UserList = styled.table`
+  width: 100%;
+  text-align: left;
+  border-collapse: collapse;
+  tr th {
+    padding: 10px 0;
+    border-bottom: 1px solid #d3d3d3;
+  }
+`;
+export const UserItem = styled.tr`
+  td {
+    padding: 10px 0;
+  }
+`;
+
+export const MenuActionItem = styled.li`
+  width: 100%;
+  padding: 10px;
+  cursor: pointer;
+  border-bottom: 1px solid #f4f4f4;
+  transition: background 0.2s;
+  a {
+    display: flex;
+    text-decoration: none;
+    color: #353535;
+    transition: color 0.2s;
+  }
+  button {
+    display: flex;
+    border: 0;
+    background: transparent;
+    color: #353535;
+    transition: color 0.2s;
+  }
+  svg {
+    margin-right: 5px;
+  }
+  &:hover {
+    background: #fff;
+    svg {
+      color: #2c3e50;
+    }
+    a {
+      color: #2c3e50;
+    }
+    button {
+      color: #2c3e50;
+    }
+  }
+`;
