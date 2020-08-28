f95cbb80fdfa743bab89715764abc937aa2a7ce7
santanarscs <raphaelstn@gmail.com>
2020-08-28 14:05:54 -0300

    feat: create initial page tasks list

---
+++ src/pages/Tasks/index.tsx
@@ -0,0 +1,128 @@
+import React, { useState } from 'react';
+
+import { FiEye, FiLink, FiMenu } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import { Header } from '../../components/Header';
+import {
+  Container,
+  Content,
+  TabGroup,
+  TabItem,
+  TaskList,
+  TaskItem,
+  MenuActionItem,
+} from './styles';
+import Dropdown from '../../components/Dropdown';
+
+export const Tasks: React.FC = () => {
+  const [tabActive, setTabActive] = useState<number>(1);
+  return (
+    <>
+      <Header />
+      <Container>
+        <h1>Lista de tarefas</h1>
+        <Content>
+          <TabGroup>
+            <TabItem
+              active={!!(tabActive === 1)}
+              onClick={() => setTabActive(1)}
+            >
+              Tarefas disponíveis
+            </TabItem>
+            <TabItem
+              onClick={() => setTabActive(2)}
+              active={!!(tabActive === 2)}
+            >
+              Tarefas em andamento
+            </TabItem>
+            <TabItem
+              onClick={() => setTabActive(3)}
+              active={!!(tabActive === 3)}
+            >
+              Tarefas concluídas
+            </TabItem>
+          </TabGroup>
+          <TaskList>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Prazo</th>
+                <th>Solicitação</th>
+                <th>Tarefa</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              <TaskItem>
+                <td />
+                <td>12/04/20</td>
+                <td>Reserva de sala</td>
+                <td>Informar quantidade de alunos</td>
+                <td>
+                  <Dropdown icon={FiMenu}>
+                    <MenuActionItem>
+                      <Link to="!">
+                        <FiEye />
+                        <span>Detalhes</span>
+                      </Link>
+                    </MenuActionItem>
+                    <MenuActionItem>
+                      <Link to="!">
+                        <FiLink />
+                        <span>Assumir</span>
+                      </Link>
+                    </MenuActionItem>
+                  </Dropdown>
+                </td>
+              </TaskItem>
+              <TaskItem>
+                <td />
+                <td>12/04/20</td>
+                <td>Reserva de sala</td>
+                <td>Informar quantidade de alunos</td>
+                <td>
+                  <Dropdown icon={FiMenu}>
+                    <MenuActionItem>
+                      <Link to="!">
+                        <FiEye />
+                        <span>Detalhes</span>
+                      </Link>
+                    </MenuActionItem>
+                    <MenuActionItem>
+                      <Link to="!">
+                        <FiLink />
+                        <span>Assumir</span>
+                      </Link>
+                    </MenuActionItem>
+                  </Dropdown>
+                </td>
+              </TaskItem>
+              <TaskItem>
+                <td />
+                <td>12/04/20</td>
+                <td>Reserva de sala</td>
+                <td>Informar quantidade de alunos</td>
+                <td>
+                  <Dropdown icon={FiMenu}>
+                    <MenuActionItem>
+                      <Link to="!">
+                        <FiEye />
+                        <span>Detalhes</span>
+                      </Link>
+                    </MenuActionItem>
+                    <MenuActionItem>
+                      <Link to="!">
+                        <FiLink />
+                        <span>Assumir</span>
+                      </Link>
+                    </MenuActionItem>
+                  </Dropdown>
+                </td>
+              </TaskItem>
+            </tbody>
+          </TaskList>
+        </Content>
+      </Container>
+    </>
+  );
+};
---
+++ src/pages/Tasks/styles.ts
@@ -0,0 +1,92 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 1100px;
+  margin: 30px auto;
+  h1 {
+    color: #353535;
+    margin-bottom: 20px;
+  }
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  background: #fff;
+  padding: 20px;
+  border-radius: 5px;
+`;
+export const TabGroup = styled.ul`
+  list-style: none;
+  display: grid;
+  grid-template-columns: repeat(3, 250px);
+  grid-gap: 20px;
+  margin-bottom: 20px;
+`;
+
+interface TabItemProps {
+  active?: boolean;
+}
+
+export const TabItem = styled.li<TabItemProps>`
+  cursor: pointer;
+  transition: color 0.2s;
+  &:hover {
+    color: #e67e22;
+  }
+  ${props =>
+    props.active &&
+    css`
+      border-bottom: 2px solid #e67e22;
+    `}
+`;
+
+export const TaskList = styled.table`
+  width: 100%;
+  text-align: left;
+  border-collapse: collapse;
+  tr th {
+    padding: 10px 0;
+    border-bottom: 1px solid #d3d3d3;
+  }
+`;
+export const TaskItem = styled.tr`
+  td {
+    padding: 10px 0;
+  }
+`;
+
+export const MenuActionItem = styled.li`
+  width: 100%;
+  padding: 10px;
+  cursor: pointer;
+  border-bottom: 1px solid #f4f4f4;
+  transition: background 0.2s;
+  a {
+    display: flex;
+    text-decoration: none;
+    color: #353535;
+    transition: color 0.2s;
+  }
+  button {
+    display: flex;
+    border: 0;
+    background: transparent;
+    color: #353535;
+    transition: color 0.2s;
+  }
+  svg {
+    margin-right: 5px;
+  }
+  &:hover {
+    background: #fff;
+    svg {
+      color: #2c3e50;
+    }
+    a {
+      color: #2c3e50;
+    }
+    button {
+      color: #2c3e50;
+    }
+  }
+`;
