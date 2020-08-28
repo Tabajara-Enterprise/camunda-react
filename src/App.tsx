ba6e935caf8b98d14d29ae134f27b81104d2138f
santanarscs <raphaelstn@gmail.com>
2020-08-28 17:45:40 -0300

    feat: using app provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from './routes';
+import AppProvider from './hooks';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyle />
-      <BrowserRouter>
+      <AppProvider>
         <Routes />
-      </BrowserRouter>
-    </>
+      </AppProvider>
+    </BrowserRouter>
   );
 };
 
