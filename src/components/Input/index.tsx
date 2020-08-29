41f41b519253aa73f5d709991d9e593a1ab0316a
santanarscs <raphaelstn@gmail.com>
2020-08-29 13:24:58 -0300

    feat: update input component
    Install unform, refactory component input and add support for new lib

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,15 +1,76 @@
-import React, { InputHTMLAttributes } from 'react';
-import { Container } from './styles';
+import React, {
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+  InputHTMLAttributes,
+} from 'react';
+import { IconBaseProps } from 'react-icons';
+import { FiAlertCircle } from 'react-icons/fi';
+import { useField } from '@unform/core';
+import { ContainerGroup, Container, Error } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
   label: string;
+  containerStyle?: React.CSSProperties;
+  icon?: React.ComponentType<IconBaseProps>;
 }
 
-export const Input: React.FC<InputProps> = ({ name, label, ...rest }) => {
+export const Input: React.FC<InputProps> = ({
+  name,
+  label,
+  containerStyle = {},
+  icon: Icon,
+  ...rest
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const [isField, setIsField] = useState<boolean>(false);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsField(!!inputRef.current?.value);
+  }, []);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
   return (
-    <Container>
+    <ContainerGroup>
       <label htmlFor={name}>{label}</label>
-      <input id={name} {...rest} />
-    </Container>
+      <Container
+        style={containerStyle}
+        isErrored={!!error}
+        isField={isField}
+        isFocused={isFocused}
+        data-testid="input-container"
+      >
+        {Icon && <Icon size={20} />}
+        <input
+          onFocus={handleInputFocus}
+          onBlur={handleInputBlur}
+          defaultValue={defaultValue}
+          ref={inputRef}
+          {...rest}
+        />
+
+        {error && (
+          <Error title={error}>
+            <FiAlertCircle color="c53030" size={20} />
+          </Error>
+        )}
+      </Container>
+    </ContainerGroup>
   );
 };
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,17 +1,73 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import Tooltip from '../Tooltip';
 
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
+export const ContainerGroup = styled.div`
+  margin-bottom: 15px;
   label {
-    margin-bottom: 5px;
     font-weight: bold;
   }
+`;
+
+interface ContainerProps {
+  isFocused: boolean;
+  isField: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+  background: transparent;
+  border-radius: 5px;
+  padding: 10px;
+  width: 100%;
+  border: 1px solid #d3d3d3;
+  color: #666360;
+  display: flex;
+  align-items: center;
+  & + div {
+    margin-top: 8px;
+  }
+  ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #3498db;
+      border-color: #3498db;
+    `}
+  ${props =>
+    props.isField &&
+    css`
+      color: #3498db;
+    `}
   input {
-    margin-bottom: 15px;
-    border: 1px solid #d3d3d3;
-    height: 50px;
-    padding: 10px;
-    border-radius: 5px;
+    flex: 1;
+    background: transparent;
+    border: 0;
+    color: #353535;
+    &::placeholder {
+      color: #666360;
+    }
+  }
+  svg {
+    margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+  svg {
+    margin: 0;
+  }
+  span {
+    background: #c53030;
+    color: #f4ede8;
+    font-size: 12px;
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
