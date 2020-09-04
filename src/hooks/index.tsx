11437226b87a09bb44d300e61d99c0e8bd29e0bd
Raphael Santana <raphaelstn@gmail.com>
2020-09-04 16:54:01 -0300

    feat: add context toast

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { KeycloakProvider } from '@react-keycloak/web';
 import keycloak from '../config/keycloak';
 import { ModalProvider } from './modal';
+import { ToastProvider } from './toast';
 
 const AppProvider: React.FC = ({ children }) => (
   <KeycloakProvider
     keycloak={keycloak}
     initConfig={{ onLoad: 'login-required' }}
   >
-    <ModalProvider>{children}</ModalProvider>
+    <ToastProvider>
+      <ModalProvider>{children}</ModalProvider>
+    </ToastProvider>
   </KeycloakProvider>
 );
 
